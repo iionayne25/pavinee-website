ab56fcf5035de7830d95e01ef429449fc83eae9e
iionayne25 <alinqweqwz@gmail.com>
2023-03-28 18:06:15 +0700

    add content in Resume page, dowload  function

--- components/btnDownload.js
+++ components/btnDownload.js
@@ -1,13 +1,19 @@
 import React from "react";
+import SaveAltIcon from "@mui/icons-material/SaveAlt";
+import {saveAs} from "file-saver"
 
 const btnDownload = () => {
+  const saveFile =()=>{
+    saveAs("ResumePDF.pdf","resume_pavinee_suthamjeam")
+    
+  }
   return (
     <>
       <button
-        className="bg-black text-greenLight rounded-full text-xs px-4 py-2 shadow-2xl border-b-4
-      border-green hover:bg-greenBtn hover:text-black"
+        className="bg-black text-greenLight block items-center rounded-full text-xs px-4 py-2 shadow-2xl border-b-4
+      border-green hover:bg-greenBtn hover:text-black" onClick={saveFile}
       >
-        Download Resume
+        <SaveAltIcon className="w-4 h-4" /> Download Resume
       </button>
     </>
   );
--- pages/resume/index.js
+++ pages/resume/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Head from "next/head";
 import Header from "@/components/Header";
+import Image from "next/image";
+import BtnDownload from "@/components/btnDownload";
+
 const index = () => {
+  // const saveFile =()=>{
+  //   saveAs("ResumePDF.pdf","pavinee_resume")
+    
+  // }
   return (
     <>
       <Head>
@@ -9,6 +16,43 @@ const index = () => {
         <link rel="icon" href="/icon.svg" />
       </Head>
       <Header />
+      <div className="h-screen">
+        <Image
+          src="/circle3.svg"
+          width={700}
+          height={700}
+          className="relative top-0 -left-14 "
+        />
+        <Image
+          src="/circle4.svg"
+          width={500}
+          height={500}
+          className="relative left-[760px] bottom-[400px] "
+        />
+        <div className="absolute top-14 left-64 ">
+          <div className="flex justify-center items-start  gap-10 ">
+            <div className="border-green items-center top-5  border-8 rounded-xl bg-white ">
+              <Image
+                src="/resume.jpg"
+                alt="Resume of Pavinee Suthamjeam"
+                width={600}
+                height={700}
+                className="rounded-xl"
+              />
+            </div >
+            <div>
+            <BtnDownload />
+            </div>
+            
+          </div>
+        </div>
+        <Image
+          src="/deco4.png"
+          width={500}
+          height={500}
+          className="relative -top-[850px] left-0 "
+        />
+      </div>
     </>
   );
 };
