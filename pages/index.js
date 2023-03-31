920868f6ecbf54ad31096310e95b27411aca8e68
iionayne25 <alinqweqwz@gmail.com>
2023-03-31 19:23:00 +0700

    add content in project page

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import TimelineCard from "@/components/TimelineCard";
 import BtnDownload from "../components/btnDownload";
 import BtnProject from "../components/btnProject";
 import Link from "next/link";
+import Footer from "@/components/footer";
 export default function Home() {
   return (
     <>
@@ -12,73 +13,90 @@ export default function Home() {
         <title>Pavinee | Home</title>
         <link rel="icon" href="/icon.svg" />
       </Head>
-      <Header/>
-      <main className="main h-fit flex-col bg-url('/circle1.svg') bg-repeat">
-      <div className="relative top-24 left-80 w-fit">
-      <Image src="/circle2.svg" width="380" height="200" className="relative 
-      " />
-      </div>
+      <Header />
+      <main className="main flex-col bg-url('/circle1.svg')">
+        <div className="relative top-24 left-80 w-fit">
+          <Image
+            src="/circle2.svg"
+            width="380"
+            height="200"
+            className="relative 
+      "
+          />
+        </div>
         <div className="flex  absolute top-16 w-screen ">
-        <div className="flex flex-col w-1/2  ">
-          <div className="text-black/50  ">
-            <p className="whitespace-nowrap overflow-hidden animate-typing ">
-              {":>"}print(Hello, Welcome to my site..)</p>
+          <div className="flex flex-col w-1/2  ">
+            <div className="text-black/50  ">
+              <p className="whitespace-nowrap overflow-hidden animate-typing ">
+                {":>"}print(Hello, Welcome to my site..)
+              </p>
+            </div>
+            <p className="text-6xl font-bold pt-4 text-green">Pavinee</p>
+            <p className="text-6xl font-bold pl-20 text-green ">Suthamjeam</p>
+            <p className="pt-5 text-black/50">
+              {":>"} Bachelor of Information Systems and Network Engineering
+            </p>
+            <p className=" text-black/50">{":>"} Chiang Mai University</p>
           </div>
-          <p className="text-6xl font-bold pt-4 text-green">Pavinee</p>
-          <p className="text-6xl font-bold pl-20 text-green ">Suthamjeam</p>
-          <p className="pt-5 text-black/50">{":>"} Bachelor of Information Systems and Network Engineering</p>
-          <p className=" text-black/50">{":>"} Chiang Mai University</p>
-        </div>
-        <div className="">
-        <TimelineCard/>
-        </div>
-        
+          <div className="">
+            <TimelineCard />
+          </div>
         </div>
         <div className="flex gap-3 relative -top-5 ">
           <div className="hover:scale-110 ">
-          <BtnDownload />
+            <BtnDownload />
           </div>
           <div className="hover:scale-110 ">
-          <Link href="/projects">
-          <BtnProject/>
-          </Link>
+            <Link href="/projects">
+              <BtnProject />
+            </Link>
           </div>
         </div>
-       
-        <div className="flex justify-center items-center gap-20 pt-8 pb-0">
+        <div className="flex justify-center items-center gap-20 pt-8 ">
           <Link href="/about">
-          <div className="flex flex-col justify-center items-center text-xs  gap-2 hover:translat-y-1 hover:scale-110 hover:text-green">
-            <Image
-            src="/profile.png" width="150" height="150"
-            className=" rounded-full"
-            />
-            <p>profile</p>
-          </div>
+            <div className="flex flex-col justify-center items-center text-xs  gap-2 hover:translat-y-1 hover:scale-110 hover:text-green">
+              <Image
+                src="/profile.png"
+                width="150"
+                height="150"
+                className=" rounded-full"
+              />
+              <p>profile</p>
+            </div>
           </Link>
           <Link href="/resume">
-          <div className="flex flex-col justify-center items-center text-xs gap-2 hover:translat-y-1 hover:scale-110 hover:text-green">
-            <Image
-            src="/fileSheet.png" width="150" height="150"
-            className=" rounded-full"
-            />
-            <p>Resume</p>
-          </div>
+            <div className="flex flex-col justify-center items-center text-xs gap-2 hover:translat-y-1 hover:scale-110 hover:text-green">
+              <Image
+                src="/fileSheet.png"
+                width="150"
+                height="150"
+                className=" rounded-full"
+              />
+              <p>Resume</p>
+            </div>
           </Link>
           <Link href="/projects">
-          <div className="flex flex-col justify-center items-center text-xs gap-2 hover:translat-y-1 hover:scale-110 hover:text-green">
-            <Image
-            src="/folder.png" width="150" height="150"
-            className=" rounded-full"
-            />
-            <p>Projects</p>
-          </div>
+            <div className="flex flex-col justify-center items-center text-xs gap-2 hover:translat-y-1 hover:scale-110 hover:text-green">
+              <Image
+                src="/folder.png"
+                width="150"
+                height="150"
+                className=" rounded-full"
+              />
+              <p>Projects</p>
+            </div>
           </Link>
         </div>
-      <div className="relative bottom-80 -left-[130px] w-fit">
-      <Image src="/deco1.png" width="200" height="200" className="opacity-70
-      " />
-      </div>
+        <div className="relative bottom-80 -left-[130px] w-fit">
+          <Image
+            src="/deco1.png"
+            width="200"
+            height="200"
+            className="opacity-70"
+          />
+        </div>
       </main>
+      <Footer/>
     </>
   );
 }
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import React from 'react'
-import Header from '@/components/Header'
+import Head from "next/head";
+import React from "react";
+import Header from "@/components/Header";
+import ProjectCard from "@/components/ProjectCard";
+import Footer from "@/components/footer";
 const index = () => {
   return (
     <>
@@ -8,9 +10,39 @@ const index = () => {
         <title>Pavinee | Projects</title>
         <link rel="icon" href="/icon.svg" />
       </Head>
-      <Header/>
-    </>
-  )
-}
+      <Header />
 
-export default index
+      <div className="main ">
+        <div className=" grid grid-cols-3 px-10 gap-10 place-items-center ">
+          <ProjectCard
+            src="/oliveLogo.png"
+            title="Senior Project"
+            name="Physical Therapy Clinic Service Platform"
+            courseName=""
+          />
+          <ProjectCard
+            src="/whisperLogo.png"
+            title="Milestone Project"
+            name="Whisper"
+            courseName="Adv. ISNE Technology Course"
+          />
+          <ProjectCard
+            src="/bearBrickLogo.png"
+            title="In Course Project"
+            name="BearBrick"
+            courseName="Fundamental Database Course"
+          />
+          <ProjectCard
+            src="/2dGame.png"
+            title="In Course Project"
+            name="2D Breakout"
+            courseName="Object Oriented Programming Course"
+          />
+        </div>
+      </div>
+      <Footer/>
+    </>
+  );
+};
+
+export default index;
